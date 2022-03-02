fb13e98f229eb33a70f29149b2b663c3313b4d9b
Mohamed Ibrahim <mohamedibrahim.mi511@gmail.com>
2022-03-02 06:35:37 +0200

    first commit

--- backend/migrations/20220302035309-quotes.js
+++ backend/migrations/20220302035309-quotes.js
@@ -19,7 +19,7 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20220226121317-quotes-up.sql');
+  var filePath = path.join(__dirname, 'sqls', '20220302035309-quotes-up.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
@@ -34,7 +34,7 @@ exports.up = function(db) {
 };
 
 exports.down = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20220226121317-quotes-down.sql');
+  var filePath = path.join(__dirname, 'sqls', '20220302035309-quotes-down.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
